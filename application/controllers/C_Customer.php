d9bd99d2c639226a024b264f65ef592ad9cd2b3d
Dionisio Febrianto <dio.721999@gmail.com>
2019-04-26 21:40:08 +0700

    ganti nama controler

--- application/controllers/C_Customer.php
+++ application/controllers/C_Customer.php
@@ -1,7 +1,7 @@
 <?php
 defined ('BASEPATH') OR exit ('No direct script access allowed');
 
-class C_loginAs extends CI_Controller {
+class C_Customer extends CI_Controller {
 
     public function __construct()
     {
@@ -9,7 +9,7 @@ class C_loginAs extends CI_Controller {
     }
 
 
-        public function index()
+        public function index() 
         {
             $this->load->view('V_loginAs');
         }
