6f7c3a3e2bc8ff87592fcd4c925249e4e2d52611
ahsanatha <ahsan.athallah@gmail.com>
2019-04-27 11:25:24 +0700

    up

--- application/controllers/C_Lelang.php
+++ application/controllers/C_Lelang.php
@@ -2,7 +2,14 @@
 defined ('BASEPATH') OR exit ('No direct script access allowed');
 
 class C_Lelang extends CI_Controller {
-    public function index(){
-        $this->load->view('V_Lelang');
+    public function __construct()
+    {
+        parent::__construct();
+        //deklarasi model
+        $this->load->model('M_Assistent');
+    }
+    public function user(){
+        $datatoview['info'] = $this->M_Assistent->getAst($_GET['id']);
+        $this->load->view('V_Lelang',$datatoview);
     }
 }
