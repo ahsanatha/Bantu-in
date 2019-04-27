5467f6528f1e56cf6c098aa22a66a99a34f09d2d
ahsanatha <ahsan.athallah@gmail.com>
2019-04-27 10:37:19 +0700

    lelang

---
+++ application/controllers/C_Lelang.php
@@ -0,0 +1,8 @@
+<?php
+defined ('BASEPATH') OR exit ('No direct script access allowed');
+
+class C_Lelang extends CI_Controller {
+    public function index(){
+        $this->load->view('V_Lelang');
+    }
+}
